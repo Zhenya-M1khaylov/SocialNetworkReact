1951aecb6a1b20bfc2a01588b320ee0f48ce8a57
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-08-21 10:14:26 +0300

    add redux form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
-const Login = () => {
+type FormDataType = {
+    login:string
+    password:string
+    rememberMe:boolean
+}
+
+export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
-        <h1>
-            Login
-        </h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field type="text" placeholder={"Login"} component={"input"} name={"login"}/>
+            </div>
+            <div>
+                <Field type="password" placeholder={"Password"} component={"input"} name={"password"}/>
+            </div>
+            <div>
+                <Field  component={"input"} type="checkbox" name={"rememberMe"}/>remeber me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     );
-};
+}
 
-export default Login;
+const LoginReduxForm = reduxForm<FormDataType>({
+    form:"login"
+})(LoginForm)
+
+export const Login = () => {
+    const onSubmit = (formData:FormDataType) => {
+        console.log(formData)
+    }
+    return (
+        <div>
+            <h1>
+                login
+            </h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+
+    );
+}
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -4,13 +4,15 @@ import dialogsReducer from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
 import {authReducer} from './auth-reducer';
 import thunkMiddleware from "redux-thunk"
+import { reducer as formReducer } from 'redux-form'
 
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form:formReducer
 })
 
 
